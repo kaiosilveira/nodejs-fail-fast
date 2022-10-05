f1edf7bb38cc652c41400c2b7c75cf139f95665d
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-05 12:18:33 +0100

    extract interface for TodoBuilder

---
+++ src/domain/entities/to-do/builder/implementation/index.ts
@@ -0,0 +1,20 @@
+import Todo from '../..';
+import ConcreteTodo from '../../implementation';
+
+export default class ConcreteTodoBuilder {
+  private _title: string;
+
+  constructor() {
+    this._title = '';
+  }
+
+  withTitle(title: string): ConcreteTodoBuilder {
+    if (!title) throw new Error('Invalid title. Expected a non-empty string.');
+    this._title = title;
+    return this;
+  }
+
+  build(): Todo {
+    return new ConcreteTodo(this._title);
+  }
+}
--- src/domain/entities/to-do/builder/index.ts
+++ src/domain/entities/to-do/builder/index.ts
@@ -1,20 +1,6 @@
 import Todo from '..';
-import ConcreteTodo from '../implementation';
 
-export default class TodoBuilder {
-  private _title: string;
-
-  constructor() {
-    this._title = '';
-  }
-
-  withTitle(title: string): TodoBuilder {
-    if (!title) throw new Error('Invalid title. Expected a non-empty string.');
-    this._title = title;
-    return this;
-  }
-
-  build(): Todo {
-    return new ConcreteTodo(this._title);
-  }
+export default interface TodoBuilder {
+  withTitle(title: string): TodoBuilder;
+  build(): Todo;
 }
