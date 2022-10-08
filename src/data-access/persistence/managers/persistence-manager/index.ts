4a76dacd00c131efaaee39b2b048efe0da7959e8
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-08 14:01:13 +0100

    delete persistance manager

--- src/data-access/persistence/managers/persistence-manager/index.ts
+++
@@ -1,7 +0,0 @@
-import Todo from '../../../../domain/entities/to-do';
-
-export default interface PersistenceManager<T> {
-  save(obj: T): Promise<string>;
-}
-
-export interface TodoPersistenceManager extends PersistenceManager<Todo> {}
--- src/data-access/persistence/saveable/index.ts
+++
@@ -1,3 +0,0 @@
-export default interface Saveable {
-  save(): Promise<string>;
-}
