0f7428d71dd33ee992d2d6a093b0a69697c1b0c7
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-08 13:39:49 +0100

    update TodoController to use TodoRepository

--- src/presentation/resources/to-do/controller/index.spec.ts
+++ src/presentation/resources/to-do/controller/index.spec.ts
@@ -1,21 +1,16 @@
-jest.mock('../../../../domain/entities/to-do/builder');
-
 import TodoController from '.';
 import * as httpCodes from '../../../enumerators/http/status-codes';
-
-import FakeTodo from '../../../../domain/entities/to-do/fake';
-import ConcreteTodo from '../../../../domain/entities/to-do/implementation';
-import FakeTodoBuilder from '../../../../domain/entities/to-do/builder/fake';
-
+import FakeTodoRepository from '../../../../data-access/repositories/to-do/fake';
 import FakeExpressFactory from '../../../../__mocks__/express/factory';
+import FakeTodo from '../../../../domain/entities/to-do/fake';
 
 describe('TodoController', () => {
   describe('create', () => {
     const userId = '123-abc';
 
-    it('should return 403 - BAD REQUEST if title is not defined', async () => {
-      const todoBuilder = new FakeTodoBuilder();
-      const ctrl = new TodoController(todoBuilder);
+    it('should return bad request if title is not defined', async () => {
+      const todoRepository = new FakeTodoRepository();
+      const ctrl = new TodoController(todoRepository);
 
       const req = FakeExpressFactory.createRequest({ headers: {}, body: { title: undefined } });
       const res = FakeExpressFactory.createResponse();
@@ -28,9 +23,9 @@ describe('TodoController', () => {
       expect(spyOnResJson).toHaveBeenCalledWith({ msg: 'Invalid to-do title' });
     });
 
-    it('should return 403 - BAD REQUEST if user id is undefined', async () => {
-      const todoBuilder = new FakeTodoBuilder();
-      const ctrl = new TodoController(todoBuilder);
+    it('should return bad request if user id is undefined', async () => {
+      const todoRepository = new FakeTodoRepository();
+      const ctrl = new TodoController(todoRepository);
 
       const req = FakeExpressFactory.createRequest({
         headers: { 'x-user-id': undefined },
@@ -47,9 +42,9 @@ describe('TodoController', () => {
       expect(spyOnResJson).toHaveBeenCalledWith({ msg: 'Invalid user identifier' });
     });
 
-    it('should return 403 - BAD REQUEST if user id is empty', async () => {
-      const todoBuilder = new FakeTodoBuilder();
-      const ctrl = new TodoController(todoBuilder);
+    it('should return bad request if user id is empty', async () => {
+      const todoRepository = new FakeTodoRepository();
+      const ctrl = new TodoController(todoRepository);
 
       const req = FakeExpressFactory.createRequest({
         headers: { 'x-user-id': '' },
@@ -69,14 +64,12 @@ describe('TodoController', () => {
     it('should create a to do', async () => {
       const id = 'abc-def';
       const title = 'Learn Typescript';
-      const todoBuilder = new FakeTodoBuilder();
-      const fakeTodo = new FakeTodo();
 
-      jest.spyOn(fakeTodo, 'save').mockImplementation(jest.fn());
-      jest.spyOn(fakeTodo, 'toJSON').mockReturnValue({ id, title });
-      jest.spyOn(todoBuilder, 'build').mockReturnValue(fakeTodo);
-      const spyOnWithTitle = jest.spyOn(todoBuilder, 'withTitle').mockReturnValue(todoBuilder);
-      const spyOnWithOwnerId = jest.spyOn(todoBuilder, 'withOwnerId').mockReturnValue(todoBuilder);
+      const createdTodo = new FakeTodo();
+      jest.spyOn(createdTodo, 'toJSON').mockReturnValue({ id, title });
+
+      const todoRepository = new FakeTodoRepository();
+      jest.spyOn(todoRepository, 'create').mockResolvedValue(createdTodo);
 
       const req = FakeExpressFactory.createRequest({
         headers: { 'x-user-id': userId },
@@ -86,25 +79,24 @@ describe('TodoController', () => {
       const res = FakeExpressFactory.createResponse();
       const spyOnResJson = jest.spyOn(res, 'json');
 
-      const ctrl = new TodoController(todoBuilder);
+      const ctrl = new TodoController(todoRepository);
       await ctrl.create(req, res);
 
-      expect(spyOnWithOwnerId).toHaveBeenCalledWith(userId);
-      expect(spyOnWithTitle).toHaveBeenCalledWith(title);
       expect(spyOnResJson).toHaveBeenCalledWith({ id, title });
     });
   });
 
   describe('list', () => {
-    it('should return 403 - BAD REQUEST if user id is not provided', () => {
-      const todoBuilder = new FakeTodoBuilder();
+    it('should return bad request if user id is not provided', async () => {
+      const todoRepository = new FakeTodoRepository();
+
       const req = FakeExpressFactory.createRequest();
       const res = FakeExpressFactory.createResponse();
       const spyOnStatusFn = jest.spyOn(res, 'status');
       const spyOnJsonFn = jest.spyOn(res, 'json');
 
-      const ctrl = new TodoController(todoBuilder);
-      ctrl.listMine(req, res);
+      const ctrl = new TodoController(todoRepository);
+      await ctrl.listMine(req, res);
 
       expect(spyOnStatusFn).toHaveBeenCalledWith(httpCodes.BAD_REQUEST);
       expect(spyOnJsonFn).toHaveBeenCalledWith({ msg: 'Invalid user identifier' });
@@ -112,24 +104,28 @@ describe('TodoController', () => {
 
     it('should return all registered to-dos for a given user', async () => {
       const userId = 'abc-123';
-      const todo1 = new ConcreteTodo({ id: '456-123', title: 'Learn TS', ownerId: userId });
-      const todo2 = new ConcreteTodo({ id: '567-423', title: 'Read TDD', ownerId: userId });
+      const todoRepository = new FakeTodoRepository();
 
-      const todoBuilder = new FakeTodoBuilder();
-      const fakeTodo = new FakeTodo();
+      const fakeTodo1 = new FakeTodo();
+      jest
+        .spyOn(fakeTodo1, 'toJSON')
+        .mockReturnValue({ id: '456-123', title: 'Learn TS', ownerId: userId });
 
-      jest.spyOn(fakeTodo, 'listByOwnerId').mockResolvedValue([todo1, todo2]);
-      jest.spyOn(todoBuilder, 'withTitle').mockReturnValue(todoBuilder);
-      jest.spyOn(todoBuilder, 'build').mockReturnValue(fakeTodo);
+      const fakeTodo2 = new FakeTodo();
+      jest
+        .spyOn(fakeTodo2, 'toJSON')
+        .mockReturnValue({ id: '567-423', title: 'Read TDD', ownerId: userId });
+
+      jest.spyOn(todoRepository, 'listByOwnerId').mockResolvedValue([fakeTodo1, fakeTodo2]);
 
       const req = FakeExpressFactory.createRequest({ headers: { 'x-user-id': userId }, body: {} });
       const res = FakeExpressFactory.createResponse();
       const spyOnJsonFn = jest.spyOn(res, 'json');
 
-      const ctrl = new TodoController(todoBuilder);
+      const ctrl = new TodoController(todoRepository);
       await ctrl.listMine(req, res);
 
-      expect(spyOnJsonFn).toHaveBeenCalledWith([todo1.toJSON(), todo2.toJSON()]);
+      expect(spyOnJsonFn).toHaveBeenCalledWith([fakeTodo1.toJSON(), fakeTodo2.toJSON()]);
     });
   });
 });
--- src/presentation/resources/to-do/controller/index.ts
+++ src/presentation/resources/to-do/controller/index.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import TodoBuilder from '../../../../domain/entities/to-do/builder';
+import TodoRepository from '../../../../data-access/repositories/to-do';
 import { BAD_REQUEST } from '../../../enumerators/http/status-codes';
 
 export default class TodoController {
-  constructor(private readonly todoBuilder: TodoBuilder) {
+  constructor(private readonly todoRepository: TodoRepository) {
     this.create = this.create.bind(this);
     this.listMine = this.listMine.bind(this);
   }
@@ -15,9 +15,7 @@ export default class TodoController {
     if (!title) return res.status(BAD_REQUEST).json({ msg: 'Invalid to-do title' });
     if (!userId) return res.status(BAD_REQUEST).json({ msg: 'Invalid user identifier' });
 
-    const todo = this.todoBuilder.withOwnerId(userId).withTitle(title).build();
-    todo.save();
-
+    const todo = await this.todoRepository.create({ title, ownerId: userId });
     return res.json(todo.toJSON());
   }
 
@@ -25,7 +23,7 @@ export default class TodoController {
     const userId = req.headers['x-user-id']?.toString();
     if (!userId) return res.status(BAD_REQUEST).json({ msg: 'Invalid user identifier' });
 
-    const result = await this.todoBuilder.build().listByOwnerId(userId);
+    const result = await this.todoRepository.listByOwnerId(userId);
     return res.json(result.map(r => r.toJSON()));
   }
 }
