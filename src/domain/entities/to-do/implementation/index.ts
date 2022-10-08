d3568577356d7b35b0dbafa4bc093cbeffef9df7
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-08 13:58:34 +0100

    stop using persistance and retrieval managers at Todo

--- src/domain/entities/to-do/implementation/index.ts
+++ src/domain/entities/to-do/implementation/index.ts
@@ -1,29 +1,20 @@
 import Todo from '..';
-import { TodoPersistenceManager } from '../../../../data-access/persistence/managers/persistence-manager';
-import { TodoRetrievalManager } from '../../../../data-access/retrieval/managers/retrieval-manager';
 
 export type ConcreteTodoProps = {
   title: string;
   ownerId: string;
   id?: string;
-  persistenceManager?: TodoPersistenceManager;
-  retrievalManager?: TodoRetrievalManager;
 };
 
 export default class ConcreteTodo implements Todo {
   private _id: string;
   private _title: string;
   private _ownerId: string;
-  private readonly _persistenceManager?: TodoPersistenceManager;
-  private readonly _retrievalManager?: TodoRetrievalManager;
 
   constructor(props: ConcreteTodoProps) {
     this._id = props.id ?? '';
     this._ownerId = props.ownerId;
     this._title = props.title;
-    this._retrievalManager = props.retrievalManager;
-
-    if (props.persistenceManager) this._persistenceManager = props.persistenceManager;
   }
 
   getOwnerId(): string {
@@ -40,31 +31,5 @@ export default class ConcreteTodo implements Todo {
 
   toJSON(): object {
     return { title: this._title, ownerId: this._ownerId };
-  }
-
-  async save(): Promise<string> {
-    if (this._persistenceManager) {
-      this._id = await this._persistenceManager?.save(this);
-      return this._id;
-    } else throw new Error(`No persistance manager was defined for to-do`);
-  }
-
-  async list(): Promise<Todo[]> {
-    if (this._retrievalManager) return this._retrievalManager?.list();
-    else throw new Error(`No retrieval manager was defined for to-do with id "${this._id}"`);
-  }
-
-  async listByOwnerId(ownerId: string): Promise<Todo[]> {
-    if (!ownerId) throw new Error('Failed to list to-do by ownerId. Invalid identifier.');
-    if (!this._retrievalManager)
-      throw new Error('Failed to list to-do by ownerId. No retrieval manager was provided.');
-    else return this._retrievalManager.listByOwnerId(ownerId);
-  }
-
-  async getById(id: string): Promise<Todo> {
-    if (!id) throw new Error('Failed to get to-do by id. Invalid identifier.');
-    if (!this._retrievalManager)
-      throw new Error('Failed to get to-do by id. No retrieval manager was provided.');
-    else return this._retrievalManager.getById(id);
   }
 }
--- src/domain/entities/to-do/index.ts
+++ src/domain/entities/to-do/index.ts
@@ -1,10 +1,7 @@
 import Serializable from '../../serialization/serializable';
-import Saveable from '../../../data-access/persistence/saveable';
-import { QueryableTodo } from '../../../data-access/retrieval/queryable';
 
-export default interface Todo extends Serializable, Saveable, QueryableTodo {
+export default interface Todo extends Serializable {
   getId(): string;
-  getById(id: string): Promise<Todo>;
   getOwnerId(): string;
   getTitle(): string;
 }
