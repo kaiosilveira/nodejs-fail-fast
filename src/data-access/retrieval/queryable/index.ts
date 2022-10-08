b52715d5a172cb03685ffef9205a9eaa81788a42
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-08 06:59:17 +0100

    introduce QueryableTodo interface

--- src/data-access/retrieval/queryable/index.ts
+++ src/data-access/retrieval/queryable/index.ts
@@ -1,4 +1,10 @@
+import Todo from '../../../domain/entities/to-do';
+
 export default interface Queryable<T> {
   list(): Promise<T[]>;
   getById(id: string): Promise<T>;
+}
+
+export interface QueryableTodo extends Queryable<Todo> {
+  listByOwnerId(ownerId: string): Promise<Todo[]>;
 }
