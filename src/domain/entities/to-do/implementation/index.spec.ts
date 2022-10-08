3aeaa747061917ad129dddd53dea3897900081bf
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-08 06:45:20 +0100

    add list operation for ConcreteTodo

--- src/domain/entities/to-do/implementation/index.spec.ts
+++ src/domain/entities/to-do/implementation/index.spec.ts
@@ -1,5 +1,7 @@
+import noop from 'lodash/noop';
 import ConcreteTodo from '.';
 import { TodoPersistenceManager } from '../../../../data-access/persistence/managers/persistence-manager';
+import { TodoRetrievalManager } from '../../../../data-access/retrieval/managers/retrieval-manager';
 
 describe('ConcreteTodo', () => {
   describe('toJSON', () => {
@@ -27,6 +29,38 @@ describe('ConcreteTodo', () => {
       await todo.save();
 
       expect(todo.getId()).toEqual(id);
+    });
+  });
+
+  describe('list', () => {
+    it('should throw an error if no persistence manager was defined for to-do when executing list', async () => {
+      const todo = new ConcreteTodo({ title: 'Temp obj', ownerId: 'system' });
+      await expect(todo.list()).rejects.toThrow(
+        `No retrieval manager was defined for to-do with id "${todo.getId()}"`
+      );
+    });
+
+    it('should return a list of to-do items', async () => {
+      const persistenceManager = { save: noop } as unknown as TodoPersistenceManager;
+      const retrievalManager = { list: noop } as unknown as TodoRetrievalManager;
+      const todoList = [
+        new ConcreteTodo({ id: '1234', title: 'Learn JS', ownerId: '234' }),
+        new ConcreteTodo({ id: '2345', title: 'Learn Typescript', ownerId: '234' }),
+        new ConcreteTodo({ id: '2346', title: 'Learn C#', ownerId: '235' }),
+      ];
+
+      jest.spyOn(retrievalManager, 'list').mockResolvedValue(todoList);
+
+      const todo = new ConcreteTodo({
+        title: 'Temp obj',
+        ownerId: 'system',
+        persistenceManager,
+        retrievalManager,
+      });
+
+      const result = await todo.list();
+
+      expect(result).toEqual(todoList);
     });
   });
 });
--- src/domain/entities/to-do/implementation/index.ts
+++ src/domain/entities/to-do/implementation/index.ts
@@ -1,11 +1,13 @@
 import Todo from '..';
 import { TodoPersistenceManager } from '../../../../data-access/persistence/managers/persistence-manager';
+import { TodoRetrievalManager } from '../../../../data-access/retrieval/managers/retrieval-manager';
 
 export type ConcreteTodoProps = {
   title: string;
   ownerId: string;
   id?: string;
   persistenceManager?: TodoPersistenceManager;
+  retrievalManager?: TodoRetrievalManager;
 };
 
 export default class ConcreteTodo implements Todo {
@@ -13,11 +15,13 @@ export default class ConcreteTodo implements Todo {
   private _title: string;
   private _ownerId: string;
   private readonly _persistenceManager?: TodoPersistenceManager;
+  private readonly _retrievalManager?: TodoRetrievalManager;
 
   constructor(props: ConcreteTodoProps) {
     this._id = props.id ?? '';
     this._ownerId = props.ownerId;
     this._title = props.title;
+    this._retrievalManager = props.retrievalManager;
 
     if (props.persistenceManager) this._persistenceManager = props.persistenceManager;
   }
@@ -44,7 +48,8 @@ export default class ConcreteTodo implements Todo {
   }
 
   async list(): Promise<Todo[]> {
-    throw new Error('Method not implemented.');
+    if (this._retrievalManager) return this._retrievalManager?.list();
+    else throw new Error(`No retrieval manager was defined for to-do with id "${this._id}"`);
   }
 
   async getById(id: string): Promise<Todo> {
