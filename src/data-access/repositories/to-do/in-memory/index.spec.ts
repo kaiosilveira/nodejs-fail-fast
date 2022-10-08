a5e86fe852307d1737052e61ce52fb0369de5523
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-08 14:54:44 +0100

    implement InMemoryTodoRepository.listByOwnerId

--- src/data-access/repositories/to-do/in-memory/index.spec.ts
+++ src/data-access/repositories/to-do/in-memory/index.spec.ts
@@ -50,4 +50,60 @@ describe('InMemoryTodoRepository', () => {
       expect(todoList).toEqual([new ConcreteTodo(todo1Json), new ConcreteTodo(todo2Json)]);
     });
   });
+
+  describe('listByOwnerId', () => {
+    afterEach(() => jest.resetAllMocks());
+
+    it('should throw an error if ownerId is empty', async () => {
+      const ownerId = '';
+      const inMemoryCache = new FakeInMemoryCache();
+      const todoRepo = new InMemoryTodoRepository(inMemoryCache);
+
+      await expect(todoRepo.listByOwnerId(ownerId)).rejects.toThrow(
+        'Failed to list all to-dos by owner id. Invalid owner identifier'
+      );
+    });
+
+    it('should return an empty array if there is items in the to-do entry in the cache', async () => {
+      const ownerId = 'abc-123';
+      const inMemoryCache = new FakeInMemoryCache();
+      jest.spyOn(inMemoryCache, 'get').mockResolvedValueOnce(undefined);
+
+      const todoRepo = new InMemoryTodoRepository(inMemoryCache);
+      const result = await todoRepo.listByOwnerId(ownerId);
+
+      expect(result).toEqual([]);
+    });
+
+    it('should throw an error if cache entry for to-dos is not an array', async () => {
+      const ownerId = 'abc-123';
+      const inMemoryCache = new FakeInMemoryCache();
+      jest.spyOn(inMemoryCache, 'get').mockResolvedValueOnce('42');
+
+      const todoRepo = new InMemoryTodoRepository(inMemoryCache);
+      await expect(todoRepo.listByOwnerId(ownerId)).rejects.toThrow(
+        'Cache has a fatal defect. To-dos entry is not an array'
+      );
+    });
+
+    it('should list all to-dos from a given owner id', async () => {
+      const ownerId = 'abc-123';
+      const todo1Data = { id: '123', title: 'Learn TS', ownerId };
+      const todo2Data = { id: '234', title: 'Learn the repository pattern', ownerId };
+      const todo3Data = { id: '456', title: 'Learn React', ownerId: 'bcd-234' };
+
+      const inMemoryCache = new FakeInMemoryCache();
+      jest
+        .spyOn(inMemoryCache, 'get')
+        .mockResolvedValueOnce(JSON.stringify([todo1Data, todo2Data, todo3Data]));
+
+      const todoRepo = new InMemoryTodoRepository(inMemoryCache);
+      const result = await todoRepo.listByOwnerId(ownerId);
+
+      expect(result).toHaveLength(2);
+      expect(result.some(todo => todo.getId() === todo1Data.id)).toBe(true);
+      expect(result.some(todo => todo.getId() === todo2Data.id)).toBe(true);
+      expect(result.some(todo => todo.getId() === todo3Data.id)).toBe(false);
+    });
+  });
 });
--- src/data-access/repositories/to-do/in-memory/index.ts
+++ src/data-access/repositories/to-do/in-memory/index.ts
@@ -3,13 +3,13 @@ import Todo from '../../../../domain/entities/to-do';
 import ConcreteTodo from '../../../../domain/entities/to-do/implementation';
 import InMemoryCache from '../../../in-memory-cache';
 
+const TO_DO_KEY = 'to-do';
 type JSONTodoData = { id: string; ownerId: string; title: string };
 
 export default class InMemoryTodoRepository implements TodoRepository {
   constructor(private readonly inMemoryCache: InMemoryCache) {}
 
   async list(): Promise<Todo[]> {
-    const TO_DO_KEY = 'to-do';
     const keyExists = await this.inMemoryCache.exists(TO_DO_KEY);
     if (!keyExists) return [];
 
@@ -27,8 +27,19 @@ export default class InMemoryTodoRepository implements TodoRepository {
     return parsedPayload.map((t: JSONTodoData) => new ConcreteTodo(t));
   }
 
-  listByOwnerId(ownerId: string): Promise<Todo[]> {
-    throw new Error('Method not implemented.');
+  async listByOwnerId(ownerId: string): Promise<Todo[]> {
+    if (!ownerId)
+      throw new Error('Failed to list all to-dos by owner id. Invalid owner identifier');
+
+    const serializedPayload = await this.inMemoryCache.get(TO_DO_KEY);
+    if (!serializedPayload) return [];
+
+    const payload = JSON.parse(serializedPayload);
+    if (!Array.isArray(payload))
+      throw new Error('Cache has a fatal defect. To-dos entry is not an array');
+
+    const todoListOfOwner = payload.filter((t: JSONTodoData) => t.ownerId === ownerId);
+    return todoListOfOwner.map((t: JSONTodoData) => new ConcreteTodo(t));
   }
 
   getById(id: string): Promise<Todo> {
